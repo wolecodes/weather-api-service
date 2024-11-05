6ec48590789cbefd9c61b4a0a5ef1d59a4f23645
wolecodes <opeyemioyeyemi141@gmail.com>
2024-11-05 16:14:59 +0100

    feat: added fucntion to fetch the weater api

--- src/weatherService.js
+++ src/weatherService.js
@@ -10,3 +10,11 @@ const createCache = async (location) => {
   const data = await client.get(cacheKey);
   return data ? JSON.parse(data) : null;
 };
+
+// fetch from the weather Api
+
+const fetchWeather = async (location) => {
+  const url = `${baseUrl}/${location}?/key=${API_KEY}`;
+  const response = await fetch(url);
+  return response;
+};
