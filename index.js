42f135f4426ded6fa104549cc36423d73e59f65e
wolecodes <opeyemioyeyemi141@gmail.com>
2024-11-05 13:38:49 +0100

    chore: install node and setup Express server

---
+++ index.js
@@ -0,0 +1,18 @@
+import express, { application } from "express";
+import { Router } from "express";
+import morgan from "morgan";
+
+const router = Router();
+const app = express();
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+//Listen to server.
+app.listen(3000, () => {
+  console.log(`Server listen at port http://localhost:3000`);
+});
