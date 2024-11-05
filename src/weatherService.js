00024ec889a5a2bd9fb6aa8261dfb34c509163d1
wolecodes <opeyemioyeyemi141@gmail.com>
2024-11-05 16:08:26 +0100

    feat: added create cache fucntion

---
+++ src/weatherService.js
@@ -0,0 +1,12 @@
+import { json } from "express";
+import client from "./client";
+
+const API_KEY = VISUAL_CROSSING_API_KEY;
+const baseUrl =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+// create Redis
+const createCache = async (location) => {
+  const cacheKey = `weather${location}`;
+  const data = await client.get(cacheKey);
+  return data ? JSON.parse(data) : null;
+};
